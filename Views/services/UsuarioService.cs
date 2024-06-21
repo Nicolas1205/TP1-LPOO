03b112cdf0e4b7f840298d49a8d39a206ea28921
Nicolas1205 <anachurinicolasdaniel@gmail.com>
2024-06-21 17:55:15 -0300

    UsuarioService and CompetenciaService

--- Views/services/UsuarioService.cs
+++ Views/services/UsuarioService.cs
@@ -5,11 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
 using System.Data;
+using ClaseBase;
 
 namespace Views.services
 {
     public class UsuarioService
     {
+        public static List<Usuario> getUsuariosByName(string nombre)
+        {
+            string query = "SELECT * FRMO dbo.Usuario WHERE Usu_NombreUsuario LIKE '%@nombre%'";
+            List<Usuario> usuarios = new();
+            using (SqlConnection conn = new(Views.Form1.ConnectionString))
+            {
+                SqlCommand cmd = new(query, conn);
+                cmd.Parameters.Add("@nombre", SqlDbType.VarChar);
+                cmd.Parameters["@nombre"].Value = nombre;
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        Usuario usuario = new();
+                        usuario.Usu_ID = (int)reader[0];
+                        usuario.Usu_NombreUsuario = reader[1].ToString();
+                        usuario.Usu_Contraseña = reader[2].ToString();
+                        usuario.Usu_ApellidoNombre = reader[3].ToString();
+                        usuario.Rol_Codigo = (int)reader[4];
+                    }
+                }
+                
+            }
+            return usuarios;
+        }
         public static string getUserRol()
         {
             string query = "SELECT Rol_Descripcion FROM dbo.Rol WHERE Rol_Codigo = @Id";
