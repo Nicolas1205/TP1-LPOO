6eef68c850487018962ec159747352312635d79a
Nicolas1205 <anachurinicolasdaniel@gmail.com>
2024-06-21 21:23:16 -0300

    UserForm Vista

---
+++ Views/views/UserForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Views.views
+{
+    partial class UserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            searchUserTextbox = new TextBox();
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // searchUserTextbox
+            // 
+            searchUserTextbox.Location = new Point(250, 76);
+            searchUserTextbox.Name = "searchUserTextbox";
+            searchUserTextbox.Size = new Size(310, 23);
+            searchUserTextbox.TabIndex = 0;
+            searchUserTextbox.TextChanged += searchUserTextbox_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(250, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(169, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Buscar por nombre de usuario:";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(165, 195);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(493, 150);
+            dataGridView1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(165, 149);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Resultados:";
+            // 
+            // UserForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Controls.Add(searchUserTextbox);
+            Name = "UserForm";
+            Text = "UserForm";
+            Load += UserForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox searchUserTextbox;
+        private Label label1;
+        private DataGridView dataGridView1;
+        private Label label2;
+    }
+}
